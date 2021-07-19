b0c5713ae6dafa8a9b89dfd1d0c57870f9026dfd
anitavttcs <anita.vertetics@hotmail.com>
2021-07-19 23:39:14 +0200

    FE small mod

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,13 +1,31 @@
+import { useState } from "react";
 import MovieCard from "../components/MovieCard";
 
 const Home = ({ user }) => {
+	const [title, setTitle] = useState("");
+
+	const search = () => {
+		console.log("search", title);
+	};
 
 	return (
 		<div className="movies">
+
+			<div className="input-group mb-3 w-75">
+				<div className="input-group-prepend">
+					<span className="input-group-text">Search for movie title:</span>
+				</div>
+				<input type="text" className="form-control" placeholder="keywords"
+					onChange={(e) => setTitle(e.target.value)}
+				/>
+				<button className="btn btn-success" onClick={search}>Search</button>
+			</div>
+
 			<MovieCard user={user} />
 			<MovieCard user={user} />
 			<MovieCard user={user} />
 			<MovieCard user={user} />
+
 		</div>
 	);
 };
